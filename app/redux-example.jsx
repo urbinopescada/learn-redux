924e926487643c7f065c68b5656ea1fcb33a3217
Urbino Pescada <urbinopescada@gmail.com>
2017-04-19 14:54:11 +0100

    create reducer for async request to IP map

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,7 +1,11 @@
 var redux = require('redux');
+var axios = require('axios');
+const $ = require('jquery');
 
 console.log('Starting redux example...');
 
+// Name reducer and action generators
+// --------------------
 var nameReducer = (state='Anonymous', action) => {
                   switch (action.type) {
                     case "CHANGE_NAME":
@@ -17,6 +21,8 @@ var changeName = (name)=>{
   }
 };
 
+// Hobbies reducer and action generators
+// --------------------
 var nextHobbyId = 1;
 var hobbiesReducer = (state=[], action) => {
                 switch (action.type) {
@@ -47,6 +53,9 @@ var removeHobbie = (id) =>{
       id
     };
 };
+
+// Movies reducer and action generators
+// --------------------
 var nextMovieId = 1;
 var moviesReducer = (state=[], action) => {
               switch (action.type) {
@@ -79,10 +88,50 @@ var removeMovie = (id) =>{
       id
     };
 };
+
+// Map reducer and action generators (asynchronous)
+// --------------------
+var mapReducer = (state={isFechting:false, url: null}, action)=>{
+  switch (action.type) {
+    case "START_LOCATION_FETCH":
+       return {isFechting: true, url: null};
+    case "COMPLETE_LOCATION_FETCH":
+       return {
+         isFechting: false,
+         url: action.url
+       };
+    default:
+      return state;
+  }
+};
+
+var startLocationFetch = () =>{
+    return {
+      type: "START_LOCATION_FETCH"
+    };
+};
+
+var completeLocationFech = (url) =>{
+    return {
+      type: "COMPLETE_LOCATION_FETCH",
+      url
+    };
+};
+var fetchLocation = () =>{
+  store.dispatch(startLocationFetch());
+  axios.get('http://ipinfo.io').then((res)=>{
+    var loc = res.data.loc;
+    var url = "http://maps.google.com?q=" +loc + "&output=embed";
+    store.dispatch(completeLocationFech(url));
+  });
+};
+// Finally combineReducers and create store
+// --------------------
 var reducer= redux.combineReducers( {
                 name: nameReducer,
                 hobbies: hobbiesReducer,
-                movies: moviesReducer
+                movies: moviesReducer,
+                map: mapReducer
             });
 
 var store = redux.createStore(reducer, redux.compose(
@@ -98,12 +147,26 @@ var store = redux.createStore(reducer, redux.compose(
 // subcribe to cjhanges
 var unsubscribe = store.subscribe(()=>{
   var state = store.getState();
-  console.log('Name is ', state.name);
-  document.getElementById('app').innerHTML = state.name;
-  console.log(state);
+  var $app = $('#app')[0];
+
+  if (state.map.isFechting) {
+    $app.innerHTML = "Loading...";
+    console.log("loading...");
+  } else {
+
+    $app.innerHTML =  '<a target="_blank" href="'+ state.map.url +
+                      '">New window to view your IP\'s physical location"</a>'+
+                      '<iframe height="900" width="100%" style="border:none;" src="'+
+                      state.map.url +'"></iframe>';
+
+    console.log("not Loading");
+  }
+  // document.getElementById('app').innerHTML = state.name;
+  //console.log(state);
 });
 // we can call unsubscribe() to stop subscrition to store changes;
 
+fetchLocation();
 store.dispatch(changeName('Urbino'));
 store.dispatch(addHobbie('Running'));
 store.dispatch(addHobbie('Travel'));
